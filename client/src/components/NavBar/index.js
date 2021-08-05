e807ee059b984d234eebae96cf3bb2aa750ddeaf
Stephen Yacas <syacas123@gmail.com>
2021-08-04 21:19:24 -0400

    made page symetrical

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -199,14 +199,14 @@ export default function PrimarySearchAppBar() {
           <div className={classes.grow} />
           <div className={classes.sectionDesktop}>
             <IconButton aria-label="show 4 new mails" color="inherit">
-              <Badge badgeContent={4} color="secondary">
+              {/* <Badge  color="secondary"> */}
                 <MailIcon />
-              </Badge>
+              {/* </Badge> */}
             </IconButton>
             <IconButton aria-label="show 17 new notifications" color="inherit">
-              <Badge badgeContent={17} color="secondary">
+              {/* <Badge  color="secondary"> */}
                 <NotificationsIcon />
-              </Badge>
+              {/* </Badge> */}
             </IconButton>
             <IconButton
               edge="end"
